40c6cd4fdb0d7d0d72cc39744e801821211ccaf1
asaikali <asaikali@gmail.com>
2023-04-17 12:08:10 +0200

    add vpc to deployment

--- tap/aws/deployment/get.sh
+++ tap/aws/deployment/get.sh
@@ -1,1 +1,10 @@
+echo "-------------------------------------------------------------------------"
+echo "Registry" 
+echo ""
 kubectl get xdeployments,xrepos,Repository.ecr.aws.upbound.io
+
+echo ""
+echo "-------------------------------------------------------------------------"
+echo "Network" 
+echo ""
+kubectl get vpc,subnet,internetgateway,defaultroutetable
